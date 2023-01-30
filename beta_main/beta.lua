e8d7ad7d32e026631f3a79af87f63aa5cc722107
ZEP1982 <90557635+ZEP1982@users.noreply.github.com>
2023-01-30 20:32:41 +0800

    Update beta.lua

--- beta_main/beta.lua
+++ beta_main/beta.lua
@@ -64,48 +64,9 @@ local Tab2 = Window:CreateTab("Scripts", 4483362458)
 local Section2 = Tab2:CreateSection("Scripts")
 
 local Button2 = Tab2:CreateButton({
-      Name = "AntiFling"
+      Name = "AntiFling",
       Callback = function()
-         local Players = game:GetService("Players")
-
-local RunService = game:GetService("RunService")
-
-local Player = Players.LocalPlayer
-
- 
-
-RunService.Stepped:Connect(function()
-
-    for _, CoPlayer in pairs(Players:GetChildren()) do
-
-        if CoPlayer ~= Player and CoPlayer.Character then
-
-            for _, Part in pairs(CoPlayer.Character:GetChildren()) do
-
-                if Part.Name == "HumanoidRootPart" then
-
-                    Part.CanCollide = false
-
-                end
-
-            end
-
-        end
-
-    end
-
- 
-
-    for _, Accessory in pairs(workspace:GetChildren()) do
-
-        if Accessory:IsA("Accessory") and Accessory:FindFirstChildWhichIsA("Part") then
-
-            Accessory:FindFirstChildWhichIsA("Part"):Destroy()
-
-        end
-
-    end
-
-end)
-end,
-      })
+         loadstring(game:HttpGet("https://pastebin.com/raw/PuAwMhXK"))()
+        end,
+      
+      
