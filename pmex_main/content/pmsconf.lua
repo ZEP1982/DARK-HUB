fa7cfe6ee44043e0682a9771b8ef444312a4388e
PracticeMedicine03 <102203580+PracticeMedicine03@users.noreply.github.com>
2023-01-31 20:43:12 +0800

    Update pmsconf.lua

--- pmex_main/content/pmsconf.lua
+++ pmex_main/content/pmsconf.lua
@@ -9,3 +9,8 @@ Purpose: PMS Data for SeaHub PMEX
 if empty there is a reason why: wip
 
 --]]
+
+local PMSEng = Window:NewTab("PMS Config")
+
+local PMSEngSec = PMSEng:NewSection("Data: pmsconf.lua")
+local PMSEngSec = PMSEng:NewSection("Runned by PMS Engine")
