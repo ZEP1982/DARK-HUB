a0da42557a752786cb0d447abd5dd9e0c56f99ff
ZEP1982 <90557635+ZEP1982@users.noreply.github.com>
2023-02-18 15:07:27 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -46,8 +46,12 @@ Tab1Section:NewButton("Infinite Yield","Created By: EdgeIY", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end) 
 
-Tab1Section:NewButton("Backdoor","Created By L1ghtningBolt", function()
+Tab1Section:NewButton("Backdoor","Created By: L1ghtningBolt", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/L1ghtingBolt/FraktureSS/master/unobfuscated.lua"))()
+end)
+
+Tab1Section:NewButton("RemoteSpy","Created By: 786", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/786/SimpleSpy/main/SimpleSpySource.lua"))()
 end)
 
 Tab1Section:NewButton("Fly GUI", "No Description", function()
@@ -257,100 +261,4 @@ local Vis = TabVis:NewSection("Visuals - RTX")
 
 Vis:NewButton("RTX", "Inject RTX", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/beJhkj3m"))()
-end)
--- Friend Join 
-		
-
-local StarterGui = game:GetService("StarterGui")
-
-local Players = game:GetService("Players")
-
-local player = Players.LocalPlayer
-
-local imageType = Enum.ThumbnailType.HeadShot
-
-local imageSize = Enum.ThumbnailSize.Size420x420
-
-local function createNotification(newPlayer)
-
-	
-
-	local success, avatarIcon, isReady = pcall(function()
-
-		return Players:GetUserThumbnailAsync(newPlayer.UserId, imageType, imageSize)
-
-	end)
-
-	
-
-	local templateTable = {
-
-		
-
-		Title = "Someone Joined You!",
-
-		Text = (newPlayer.Name.." is here!"),
-
-		Icon = "rbxthumb://type=AvatarHeadShot&id=1&w=420&h=420",
-
-		Duration = 10
-
-	}
-
-	
-
-	if success and avatarIcon and isReady then
-
-		templateTable["Icon"] = avatarIcon
-
-	end
-
-	
-
-	StarterGui:SetCore("SendNotification", templateTable)
-
-end
-
-Players.PlayerAdded:Connect(function(newPlayer)
-
-	
-
-	local retryAttempts = 5
-
-	local retryDelay = 1
-
-	
-
-	local success, friendCheck
-
-	
-
-	
-
-	while not success and retryAttempts > 0 do
-
-		
-
-		success, friendCheck = pcall(function()
-
-			return player:IsFriendsWith(newPlayer.UserId)
-
-		end)
-
-		
-
-		retryAttempts -= 1
-
-		wait(retryDelay)
-
-	end
-
-	
-
-	if success and friendCheck then
-
-		createNotification(newPlayer)
-
-	end
-
 end)
