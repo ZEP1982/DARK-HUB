31cabe59c78c2f0983705df789a7537c2a23d206
PracticeMedicine03 <102203580+PracticeMedicine03@users.noreply.github.com>
2023-02-19 16:06:37 +0800

    Changed theme

--- pmex_main/main.lua
+++ pmex_main/main.lua
@@ -8,7 +8,7 @@
 
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 
-local Window = Library.CreateLib("SeaHub | PMEX","BloodTheme")
+local Window = Library.CreateLib("SeaHub | PMEX","Synapse")
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
 
