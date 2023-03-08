e2934b40474ccfa745661befc99e6174b6a3d29d
ZEP1982 <90557635+ZEP1982@users.noreply.github.com>
2023-03-08 22:58:32 +0800

    Update main.lua

--- launcher_main/main.lua
+++ launcher_main/main.lua
@@ -16,6 +16,15 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 
 })
 
+game:GetService("StarterGui"):SetCore("SendNotification",{
+
+	Title = "New Executor", -- Required
+
+	Text = "Do You Want To Execute Arceus X?", -- Required
+
+
+
+})
 -- Tabs
 
 local TabHome = Window:NewTab("Home")
